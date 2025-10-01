84318247237afe79f8f44893832ae78ce79ef328
Fortunate-ak <tiffanysharks17@gmail.com>
2025-10-01 17:12:59 +0200

    mineapp fisrt commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,35 @@
+import {useState} from 'react';
+
+function Square({value, onSquareClick}){
+    return(
+        <button className="square" onClick={onSquareClick} >
+            {value}
+        </button>
+    );
+}
+
+function Board({xIsNext, squares, onPlay}) {
+    function handleClick(i){
+        if (calculateWinner(squares)|| squares[i]){
+            return;
+        }
+        const nextSquares = squares.slice();
+        if (xIsNext){
+            nextSquares[i] = 'X';
+        } else {
+            nextSquares[i] = 'O';
+        }
+        onPlay(nextSquares);
+    }
+
+    const winner = calculateWinner(squares);
+    let status;
+    if (winner){
+        status = 'Winner: ' + winner;
+    } else {
+        status = 'Next Playe: ' + (xIsNext ? 'X' : 'O');
+    
+    }
+    }        
+    
 

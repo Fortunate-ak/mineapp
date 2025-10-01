4ee3ab106f74a4d424ea127ed31e12a53216d0eb
Fortunate-ak <tiffanysharks17@gmail.com>
2025-10-01 20:54:08 +0200

    fixing error Square

--- src/App.jsx
+++ src/App.jsx
@@ -34,15 +34,19 @@ function Board({xIsNext, squares, onPlay}) {
         <>
         <div className="status">{status}</div>
         <div className="board-row">
-            <square value={squares[0]} onSquareClick={() => handleClick(0)} />
-            <square value={squares[1]} onSquareClick={() => handleClick(1)} />
-            <square value={squares[2]} onSquareClick={() => handleClick(2)} />
-            <square value={squares[3]} onSquareClick={() => handleClick(3)} />
-            <square value={squares[4]} onSquareClick={() => handleClick(4)} />
-            <square value={squares[5]} onSquareClick={() => handleClick(5)} />
-            <square value={squares[6]} onSquareClick={() => handleClick(6)} />
-            <square value={squares[7]} onSquareClick={() => handleClick(7)} />
-            <square value={squares[8]} onSquareClick={() => handleClick(8)} />
+            <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+            <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+            <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+        </div>
+        <div className="board-row">
+            <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+            <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+            <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+        </div>
+        <div className="board-row">
+            <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+            <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+            <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
         </div>
         </>
     );
